fcfacbe5d5c7cbeff52a77b3e2cc503f3bb4fcb4
Youssef Walid <yousefwalid98@hotmail.com>
2021-05-22 22:32:32 +0200

    Initial draft (not working)

---
+++ fully-connected/src/DMA.do
@@ -0,0 +1,34 @@
+vsim work.DMA
+# vsim work.DMA 
+# Start time: 01:57:10 on May 22,2021
+# Loading sv_std.std
+# Loading work.DMA
+add wave -position insertpoint  \
+sim:/DMA/read \
+sim:/DMA/o_ready \
+sim:/DMA/o_mem_addr \
+sim:/DMA/o_buffer \
+sim:/DMA/MEM_WIDTH \
+sim:/DMA/mem_idx \
+sim:/DMA/MEM_ADDRESS_WIDTH \
+sim:/DMA/mem_addr \
+sim:/DMA/i_read \
+sim:/DMA/i_mem_data \
+sim:/DMA/i_count \
+sim:/DMA/i_address \
+sim:/DMA/COUNT_WIDTH \
+sim:/DMA/count \
+sim:/DMA/clk \
+sim:/DMA/BUFFER_SIZE \
+sim:/DMA/address
+force -freeze sim:/DMA/clk 1 0, 0 {200 ps} -r 400
+force -freeze sim:/DMA/i_address 0 0
+force -freeze sim:/DMA/i_count 111 0
+force -freeze sim:/DMA/i_mem_data 10 0
+run
+run
+run
+force -freeze sim:/DMA/i_read 1 0
+run
+run
+run
---
+++ fully-connected/src/DMA.sv
@@ -0,0 +1,55 @@
+module DMA #(parameter BUFFER_SIZE = 20, parameter MEM_ADDRESS_WIDTH=10, parameter COUNT_WIDTH=16, parameter MEM_WIDTH=16)(
+  input   reg                         i_read,
+  input   [MEM_ADDRESS_WIDTH-1 : 0]   i_address,
+  input   reg [COUNT_WIDTH-1 : 0]     i_count,
+  input   reg                         clk,
+
+  input   [MEM_WIDTH-1:0]             i_mem_data,
+  output  [MEM_ADDRESS_WIDTH-1 : 0]   o_mem_addr,
+
+  output  reg [MEM_WIDTH-1 : 0]       o_buffer [BUFFER_SIZE-1 : 0],
+  output  reg                         o_ready
+);
+
+  reg read;
+  reg [COUNT_WIDTH-1 : 0] count;
+  reg [MEM_ADDRESS_WIDTH-1 : 0] address;
+  reg [MEM_ADDRESS_WIDTH-1 : 0] mem_addr;
+
+  integer mem_idx;
+
+  always @(posedge i_read, clk) begin
+
+    @(posedge i_read) begin
+      read = i_read;
+      count = $unsigned(i_count);
+      address = i_address;
+      mem_idx = 0;
+    end
+
+    @(posedge clk) begin
+      if (read == 1) begin
+        if($unsigned(mem_idx) < $unsigned(count)) begin
+          mem_addr = (mem_idx + address);
+        end else begin
+          read = 0;
+          o_ready = 1;
+        end
+      end
+    end
+
+    @(negedge clk) begin
+      if(read == 1) begin
+        if($unsigned(mem_idx) < $unsigned(count)) begin
+          o_buffer[mem_idx] <= i_mem_data;
+          mem_idx++;
+        end
+      end
+      o_ready = 0;
+    end
+
+  end
+
+  assign o_mem_addr = mem_addr;
+
+endmodule
